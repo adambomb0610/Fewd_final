41534fe947c1ff15d7c4c6dd9767f6768380e0e8
adambomb0610 <APBellinger@msc-abelligner-mac.man.co>
2016-10-27 19:35:09 -0400

    cool stuff

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+$("#images, body").vegas({
+    slides: [
+        { src: "images/san_fran.jpg" },
+        { src: "/img/slide2.jpg" },
+        { src: "/img/slide3.jpg" },
+        { src: "/img/slide4.jpg" }
+    ]
+});
